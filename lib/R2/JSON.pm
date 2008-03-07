4dcc9f0419ae11b43c9c96986182a7fade9926a8
autarch <devnull@localhost>
2008-03-07 20:41:54 +0000

    Add error handling plugin more or less copied from VegGuide. Needs examination to make generic.

---
+++ lib/R2/JSON.pm
@@ -0,0 +1,20 @@
+package R2::JSON;
+
+use strict;
+use warnings;
+
+use JSON::XS;
+
+
+{
+    my $json = JSON::XS->new();
+    $json->pretty(1);
+    $json->utf8(1);
+
+    sub Encode { $json->encode( $_[1] ) }
+
+    sub Decode { $json->decode( $_[1] ) }
+}
+
+
+1;
---
+++ lib/R2/Plugin/ErrorHandling.pm
@@ -0,0 +1,106 @@
+package R2::Plugin::ErrorHandling;
+
+use strict;
+use warnings;
+
+use HTTP::Status qw( RC_NOT_FOUND RC_INTERNAL_SERVER_ERROR );
+use R2::JSON;
+
+
+# I'd really rather _not_ copy this whole thing in here, but it's the
+# only way to override how errors are logged. I have to monkey-patch
+# rather than subclassing or else NEXT::finalize() ends up calling the
+# finalize in Catalyst itself before calling finalize() for other
+# plugins (a mess!).
+{
+    package Catalyst;
+
+    no warnings 'redefine';
+sub finalize {
+    my $self = shift;
+
+    $self->NEXT::finalize(@_);
+
+    for my $error ( @{ $self->error } ) {
+        $self->_log_error($error);
+    }
+
+    # Allow engine to handle finalize flow (for POE)
+    if ( $self->engine->can('finalize') ) {
+        $self->engine->finalize($self);
+    }
+    else {
+
+        $self->finalize_uploads;
+
+        # Error
+        if ( $#{ $self->error } >= 0 ) {
+            $self->finalize_error;
+        }
+
+        $self->finalize_headers;
+
+        # HEAD request
+        if ( $self->request->method eq 'HEAD' ) {
+            $self->response->body('');
+        }
+
+        $self->finalize_body;
+    }
+    
+    if ($self->use_stats) {        
+        my $elapsed = sprintf '%f', $self->stats->elapsed;
+        my $av = $elapsed == 0 ? '??' : sprintf '%.3f', 1 / $elapsed;
+        $self->log->info(
+            "Request took ${elapsed}s ($av/s)\n" . $self->stats->report . "\n" );        
+    }
+
+    return $self->response->status;
+}
+}
+sub _log_error
+{
+    my $self  = shift;
+    my $error = shift;
+
+    # XXX - change this later to log to the apache log?
+#    if ( $error =~ /unknown resource/ )
+
+    my %error = ( uri => $self->request()->uri() . '' );
+
+    if ( my $user = $self->user() )
+    {
+        $error{user} = $user->email_address();
+        $error{user} .= q{ - } . $user->user_id()
+            if $user->user_id();
+    }
+
+    $error{error} = $error . '';
+
+    $self->log()->error( R2::JSON->Encode( \%error ) );
+}
+
+sub finalize_error
+{
+    my $self = shift;
+
+    if ( $self->debug() )
+    {
+        $self->NEXT::finalize_error( $self, @_ );
+        return;
+    }
+
+    my @errors = @{ $self->error() || [] };
+
+    my $status =
+        ( grep { /unknown resource|no default/i } @errors ) ? RC_NOT_FOUND : RC_INTERNAL_SERVER_ERROR;
+
+    $self->error( [] );
+
+    $self->response()->content_type('text/html; charset=utf-8');
+    $self->response()->status($status);
+    $self->response()->body( $self->subreq( "/error/$status" ) );
+}
+
+
+1;
