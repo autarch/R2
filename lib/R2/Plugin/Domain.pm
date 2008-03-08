33e6b2099b773b174e4ab22e8a07a2e85f3023d6
autarch <devnull@localhost>
2008-03-08 16:56:25 +0000

    plugin to get the domain for the host

---
+++ lib/R2/Plugin/Domain.pm
@@ -0,0 +1,27 @@
+package R2::Plugin::Domain;
+
+use strict;
+use warnings;
+
+use R2::Model::Domain;
+
+
+sub domain
+{
+    my $self = shift;
+
+    return $self->{'R2::Plugin::Domain::domain'} ||= $self->_domain_for_request();
+}
+
+sub _domain_for_request
+{
+    my $self = shift;
+
+    my $host = $self->request()->uri()->host();
+    my $domain = R2::Domain->new( web_hostname => $host )
+        or die "No domain found for hostname ($host)\n";
+
+    return $domain;
+}
+
+1;
