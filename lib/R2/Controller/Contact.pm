3e5630eda76a93de74130e5528323b5fdfe74b97
autarch <devnull@localhost>
2008-03-18 19:23:19 +0000

    start of Contact controller

---
+++ lib/R2/Controller/Contact.pm
@@ -0,0 +1,27 @@
+package R2::Controller::Contact;
+
+use strict;
+use warnings;
+
+use base 'R2::Controller::Base';
+
+
+sub new_contact_form : Local
+{
+    my $self = shift;
+    my $c    = shift;
+
+    unless ( $c->model('Authz')->user_can_add_contact( user    => $c->user(),
+                                                       account => $c->user()->account(),
+                                                     ) )
+    {
+        $c->_redirect_with_error
+            ( error => 'You are not allowed to add contacts',
+              uri   => $c->uri_for('/'),
+            );
+    }
+
+    $c->stash()->{template} = '/contact/new-contact-form';
+}
+
+1;
