86d37de38a273dfa80b0aa8ad9ee19c797b9ed59
autarch <devnull@localhost>
2008-04-12 22:39:14 +0000

    Start implementing contact (person) view.
    Move new-person-form to new dir.

--- lib/R2/Controller/Contact.pm
+++ lib/R2/Controller/Contact.pm
@@ -6,6 +6,7 @@ use warnings;
 use base 'R2::Controller::Base';
 
 use R2::Schema;
+use R2::Schema::Contact;
 use R2::Schema::Person;
 
 
@@ -24,7 +25,7 @@ sub new_person_form : Local
             );
     }
 
-    $c->stash()->{template} = '/contact/new-person-form';
+    $c->stash()->{template} = '/person/new-person-form';
 }
 
 sub new_contact : Path('/contact') : ActionClass('+R2::Action::REST') { }
@@ -34,7 +35,18 @@ sub new_contact_POST
     my $self = shift;
     my $c    = shift;
 
+    unless ( $c->model('Authz')->user_can_add_contact( user    => $c->user(),
+                                                       account => $c->user()->account(),
+                                                     ) )
+    {
+        $c->_redirect_with_error
+            ( error => 'You are not allowed to add contacts',
+              uri   => $c->uri_for('/'),
+            );
+    }
+
     my %p = $c->request()->person_params();
+    $p{account_id} = $c->user()->account_id();
 
     my $person;
     my $insert_sub =
@@ -45,16 +57,66 @@ sub new_contact_POST
 
     eval { R2::Schema->RunInTransaction($insert_sub) };
 
-    if ( my $e = $@ )
+    if ( my $e = R2::Exception::DataValidation->caught() )
     {
         $c->_redirect_with_error( error  => $e,
                                   uri    => '/contact/new_person_form',
                                   params => $c->request()->params(),
                                 );
     }
+    elsif ($@)
+    {
+        die $@;
+    }
 
     $c->redirect_and_detach( $c->uri_for( '/contact/' . $person->contact_id() ) );
 }
 
+sub _set_contact : Chained('/') : PathPart('contact') : CaptureArgs(1)
+{
+    my $self       = shift;
+    my $c          = shift;
+    my $contact_id = shift;
+
+    my $contact = R2::Schema::Contact->new( contact_id => $contact_id );
+
+    $c->redirect_and_detach('/')
+        unless $contact;
+
+    unless ( $c->model('Authz')->user_can_view_contact( user    => $c->user(),
+                                                        contact => $contact,
+                                                      ) )
+    {
+        $c->_redirect_with_error
+            ( error => 'You are not authorized to view this contact',
+              uri   => $c->uri_for('/'),
+            );
+    }
+
+    $c->stash()->{contact} = $contact;
+}
+
+sub contact : Chained('_set_contact') : PathPart('') : Args(0) : ActionClass('+R2::Action::REST') { }
+
+sub contact_GET_html : Private
+{
+    my $self = shift;
+    my $c    = shift;
+
+    my $contact = $c->stash()->{contact};
+
+    my $meth = '_display_' . lc $contact->contact_type();
+    $self->$meth($c);
+}
+
+sub _display_person
+{
+    my $self = shift;
+    my $c    = shift;
+
+    $c->stash()->{person} = $c->stash()->{contact}->person();
+
+    $c->stash()->{template} = '/person/view';
+}
 
 1;
