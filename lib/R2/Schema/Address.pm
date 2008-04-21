adb77722ffd1d75df67db001d628178aeb5e11df
autarch <devnull@localhost>
2008-04-21 16:36:42 +0000

    add city_region_postal_code method

--- lib/R2/Schema/Address.pm
+++ lib/R2/Schema/Address.pm
@@ -6,6 +6,7 @@ use warnings;
 use R2::Schema::AddressType;
 use R2::Schema::Contact;
 use R2::Schema;
+use R2::Util qw( string_is_empty );
 
 use Fey::ORM::Table;
 
@@ -18,7 +19,28 @@ use Fey::ORM::Table;
 
     has_one 'type' =>
         ( table => $schema->table('AddressType') );
+
+    has_one( $schema->table('Country') );
+
+    has 'city_region_postal_code' =>
+        ( is      => 'ro',
+          isa     => 'Str|Undef',
+          lazy    => 1,
+          builder => '_build_city_region_postal_code',
+        );
 }
+
+sub _build_city_region_postal_code
+{
+    my $self = shift;
+
+    my $c_r_pc = join ', ', grep { ! string_is_empty($_) } $self->city(), $self->region();
+    $c_r_pc .= q{ } if $c_r_pc;
+    $c_r_pc .= $self->postal_code() if ! string_is_empty( $self->postal_code() );
+
+    return $c_r_pc;
+}
+
 
 no Fey::ORM::Table;
 no Moose;
