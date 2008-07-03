46794e8c42720641231aeea5c8744d928e4f061a
Dave Rolsky <autarch@urth.org>
2008-07-03 21:34:57 +0800

    Load R2::Types here for the benefit of all the table classes

--- lib/R2/Schema.pm
+++ lib/R2/Schema.pm
@@ -8,6 +8,8 @@ use Fey::ORM::Schema;
 use Fey::DBIManager::Source;
 use Fey::Loader;
 use R2::Config;
+# Loading this for the sake of all the table classes which need it.
+use R2::Types;
 
 
 my $source;
