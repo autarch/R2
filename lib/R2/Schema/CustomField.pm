3d09b23027a3ec7ae1245b2b6065e5df696bb776
Dave Rolsky <autarch@urth.org>
2009-02-21 09:23:20 -0600

    Implement value_count and is_deletable for CustomField objects

--- lib/R2/Schema/CustomField.pm
+++ lib/R2/Schema/CustomField.pm
@@ -25,8 +25,62 @@ with 'R2::Role::DataValidator';
     transform 'type'
         => inflate { R2::CustomFieldType->new( type => $_[1] ) }
         => deflate { blessed $_[1] ? $_[1]->type() : $_[1] };
+
+    has 'value_count' =>
+        ( is         => 'ro',
+          isa        => 'Int',
+          lazy_build => 1,
+        );
+
+    has '_value_count_select' =>
+        ( is         => 'ro',
+          isa        => 'Fey::SQL::Select',
+          lazy_build => 1,
+        );
 }
 
+
+sub is_deletable
+{
+    my $self = shift;
+
+    return $self->value_count() ? 0 : 1;
+}
+
+sub _build_value_count
+{
+    my $self = shift;
+
+    my $select = $self->_value_count_select();
+
+    my $dbh = $self->_dbh($select);
+
+    my $row =
+        $dbh->selectrow_arrayref( $select->sql($dbh), {}, $select->bind_params() );
+
+    return $row ? $row->[0] : 0;
+}
+
+sub _build__value_count_select
+{
+    my $self = shift;
+
+    my $select = R2::Schema->SQLFactoryClass()->new_select();
+
+    my $schema = R2::Schema->Schema();
+
+    my $value_table = $schema->table( $self->type()->table() );
+
+    my $count =
+        Fey::Literal::Function->new( 'COUNT', $value_table->column('custom_field_id') );
+
+    $select->select($count)
+           ->from($value_table)
+           ->where( $value_table->column('custom_field_id'),
+                    '=', $self->custom_field_id() );
+
+    return $select;
+}
 
 no Fey::ORM::Table;
 no MooseX::ClassAttribute;
