c2daaa42d1adcee8997662d17fa0d5f135150bde
Dave Rolsky <autarch@urth.org>
2010-10-03 10:35:11 -0500

    update scripts

--- bin/r2_test.pl
+++ bin/r2_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'R2';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('R2', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ r2_test.pl - Catalyst Test
 r2_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    r2_test.pl http://localhost/some_action
