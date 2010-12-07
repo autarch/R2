704b79418bdd1a9e11539655b4e9ac8c5b00b269
Dave Rolsky <autarch@urth.org>
2010-12-07 13:01:03 -0600

    Copy policy from Silki

--- lib/R2/Schema/Policy.pm
+++ lib/R2/Schema/Policy.pm
@@ -4,29 +4,68 @@ use strict;
 use warnings;
 
 use DateTime::Format::Pg;
+use Encode qw( decode );
 use Lingua::EN::Inflect qw( PL_N );
 use Scalar::Util qw( blessed );
 
 use Fey::ORM::Policy;
 
-transform_all matching { $_[0]->name() =~ /_datetime$/ } => deflate {
-    blessed $_[1] ? DateTime::Format::Pg->format_datetime( $_[1] ) : $_[1];
-},
+#<<<
+transform_all
+    matching { $_[0]->name() =~ /_datetime$/ }
+    => deflate {
+        blessed $_[1] && $_[1]->isa('DateTime')
+            ? DateTime::Format::Pg->format_datetime( $_[1] )
+            : $_[1];
+    }
     => inflate {
-    return $_[1] unless defined $_[1];
-    my $dt = DateTime::Format::Pg->parse_datetime( $_[1] );
-    $dt->set_time_zone('UTC');
-    return $dt;
+        return $_[1] unless defined $_[1];
+        my $dt = DateTime::Format::Pg->parse_datetime( $_[1] );
+        $dt->set_time_zone('UTC');
+        return $dt;
     };
-
-transform_all matching { $_[0]->name() =~ /_date$/ } => deflate {
-    blessed $_[1] ? DateTime::Format::Pg->format_date( $_[1] ) : $_[1];
-}, => inflate {
-    return $_[1] unless defined $_[1];
-    my $dt = DateTime::Format::Pg->parse_date( $_[1] );
-    $dt->set_time_zone('UTC');
-    return $dt;
-};
+#>>>
+#<<<
+transform_all
+    matching { $_[0]->name() =~ /_datetime$/ }
+    => deflate {
+        blessed $_[1] && $_[1]->isa('DateTime')
+            ? DateTime::Format::Pg->format_datetime( $_[1] )
+            : $_[1];
+    }
+    => inflate {
+        return $_[1] unless defined $_[1];
+        my $dt = DateTime::Format::Pg->parse_datetime( $_[1] );
+        $dt->set_time_zone('UTC');
+        return $dt;
+    };
+#>>>
+#<<<
+transform_all
+    matching { $_[0]->name() =~ /_date$/ } =>
+    deflate {
+        blessed $_[1] && $_[1]->isa('DateTime')
+            ? DateTime::Format::Pg->format_date( $_[1] )
+            : $_[1];
+    }
+    => inflate {
+        return $_[1] unless defined $_[1];
+        my $dt = DateTime::Format::Pg->parse_date( $_[1] );
+        $dt->set_time_zone('UTC');
+        return $dt;
+    };
+#>>>
+# This is a hack that should not be necessary, but DBD::Pg has a fixed list of
+# column types it will treat as utf-8, and user-defined types are not
+# included. See https://rt.cpan.org/Ticket/Display.html?id=40199 for details.
+my %text_types = map { $_ => 1 } qw( citext email_address filename );
+#<<<
+transform_all
+    matching { $text_types{ lc $_[0]->type() } } =>
+    inflate {
+        return decode( 'utf-8', $_[1] );
+    };
+#>>>
 
 has_one_namer {
     my $name = $_[0]->name();
@@ -43,3 +82,7 @@ has_many_namer {
 
     return join q{_}, @parts;
 };
+
+1;
+
+# ABSTRACT: A Fey::Policy for R2
