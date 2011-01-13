9da0942ab6e1d724f83aca8a467b14485c169b82
Dave Rolsky <autarch@urth.org>
2011-01-13 14:45:31 -0600

    Remove $() function

--- share/js-source/DOM/Utils.js
+++ share/js-source/DOM/Utils.js
@@ -10,43 +10,10 @@ if ( typeof( DOM ) == 'undefined' ) {
 
 */
 
-DOM.Utils = {
-    EXPORT: [ '$' ]
-   ,'$' : function () {
-        var elements = new Array();
-
-        for (var i = 0; i < arguments.length; i++) {
-            var element = arguments[i];
-
-            if (typeof element == 'string')
-                element = document.getElementById(element)
-                    || document.getElementsByName(element)[0]
-//                    || document.getElementsByTagName(element)[0]
-                    || undefined
-                ;
-
-            if (arguments.length == 1) 
-                return element;
-
-            elements.push( element );
-        }
-
-        return elements;
-    }
-};
-
-/* Needed to get this working without real exporting */
-window["$"] = DOM.Utils["$"];
-$ = window["$"];
-
-/*
-
-*/
-
 document.getElementsByClass = function(className) {
     var children = document.getElementsByTagName('*') || document.all;
     var elements = new Array();
-  
+
     for (var i = 0; i < children.length; i++) {
         var child = children[i];
         var classNames = child.className.split(' ');
@@ -57,7 +24,7 @@ document.getElementsByClass = function(className) {
             }
         }
     }
-  
+
     return elements;
 };
 document.getElementsByClassName = document.getElementsByClass;
