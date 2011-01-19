c6f5473348ea83f893a539b1debe2bf368e852f4
Dave Rolsky <autarch@urth.org>
2011-01-19 13:51:20 -0600

    Require that User account_id & role_id are both null or both not null together

--- schema/R2.sql
+++ schema/R2.sql
@@ -27,7 +27,11 @@ CREATE TABLE "User" (
        account_id               INT8            NULL,
        role_id                  INTEGER         NULL,
        CONSTRAINT valid_username CHECK ( username != '' ),
-       CONSTRAINT valid_password CHECK ( password != '' )
+       CONSTRAINT valid_password CHECK ( password != '' ),
+       CONSTRAINT account_requires_role
+                  CHECK ( ( account_id IS NULL AND role_id IS NULL )
+                          OR
+                          ( account_id IS NOT NULL AND role_id IS NOT NULL ) )
 );
 
 CREATE TABLE "Account" (
