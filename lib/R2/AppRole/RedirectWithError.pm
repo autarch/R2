ec1cec6210d9f88ea98c0cf7e4aa5a252795a152
Dave Rolsky <autarch@urth.org>
2011-01-20 10:14:40 -0600

    Need to check whether errors should be shown to user somewhere else

--- lib/R2/AppRole/RedirectWithError.pm
+++ lib/R2/AppRole/RedirectWithError.pm
@@ -7,11 +7,9 @@ use namespace::autoclean;
 use HTTP::Status qw( RC_OK );
 use JSON::XS;
 
-use Carp qw( croak );
 use Moose::Role;
 use MooseX::Params::Validate qw( validated_hash );
 use R2::Types qw( ErrorForSession URIStr HashRef );
-use Scalar::Util qw( blessed );
 
 # These are not available yet?
 #requires qw( redirect_and_detach session_object );
@@ -31,10 +29,6 @@ sub redirect_with_error {
     my %p = validated_hash( \@_, %spec );
 
     die "Must provide a form or error" unless $p{error} || $p{form};
-
-    croak $p{error}
-        unless blessed $p{error}
-            && $p{error}->isa('R2::Exception::DataValidation');
 
     $self->session_object()->add_error( $p{error} )
         if $p{error};
