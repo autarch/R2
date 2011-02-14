0433732d228ff8ac79f110ec6cecd4112592d892
Dave Rolsky <autarch@urth.org>
2011-02-14 13:49:05 -0600

    move contacts list to Contact controller

--- lib/R2/Controller/Contact.pm
+++ lib/R2/Controller/Contact.pm
@@ -47,6 +47,22 @@ for my $type (qw( person household organization )) {
     };
 }
 
+get_html 'contacts'
+    => chained '/account/_set_account'
+    => args 0
+    => sub {
+    my $self = shift;
+    my $c    = shift;
+
+    $c->stash()->{search} = R2::Search::Contact->new(
+        account => $c->account(),
+        limit   => 20,
+        page    => 1,
+    );
+
+    $c->stash()->{template} = '/search/contact_list';
+};
+
 get q{}
     => chained '/account/_set_account'
     => path_part 'contact'
