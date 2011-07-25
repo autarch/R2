de419cf9bedee912e592abcd67733b91d5e46687
Dave Rolsky <autarch@urth.org>
2011-07-25 14:01:39 -0500

    Make spinner code smarter - don't add spinner if we already have one. Be smarter about removing spinner as well.

--- share/js-source/R2/Role/Spinner.js
+++ share/js-source/R2/Role/Spinner.js
@@ -1,15 +1,15 @@
-JSAN.use('R2.Role.CurriesThis');
+JSAN.use("R2.Role.CurriesThis");
 
 Role(
-    'R2.Role.Spinner', {
+    "R2.Role.Spinner", {
         does: R2.Role.CurriesThis,
         my: {
             has: {
                 Spinner: {
-                    is:        'roc',
+                    is:        "roc",
                     isPrivate: true,
-                    lazy:      '_BuildSpinner'
-                }
+                    lazy:      "_BuildSpinner"
+                },
             },
             methods: {
                 _BuildSpinner: function () {
@@ -29,26 +29,34 @@ Role(
         },       
         has: {
             spinner: {
-                is:        'rwc',
+                is:        "rwc",
+                isPrivate: true
+            },
+            spinner_id: {
+                is:        "roc",
                 isPrivate: true,
+                builder:   "_buildspinner_id"
             }
         },
         methods: {
-            /* The reason for doing this on a delay is that if the server
-               response for an AJAX response is extremely fast, the spinner
-               image flashes and disappears, which looks weird. */
+            _buildspinner_id: function () {
+                return "JS-" + ( typeof this ) + "-spinner";
+            },
             _showSpinnerIn: function (elt) {
-                if ( ! elt.length ) {
+                if ( ! this._elt_needs_spinner(elt) ) {
                     return;
                 }
 
-                elt.children().detach();
-                elt.append( this.my.Spinner() );
+                var spinner = this.my.Spinner().clone();
+                spinner.attr( "id", this.spinner_id() );
 
-                this.spinner( this.my.Spinner() );
+                elt.children().detach();
+                elt.append(spinner);
+
+                this.spinner(spinner);
             },
             _showSpinnerOver: function (elt) {
-                if ( ! elt.length ) {
+                if ( ! this._elt_needs_spinner(elt) ) {
                     return;
                 }
 
@@ -60,6 +68,7 @@ Role(
 
                 container.css( "height", elt.height() );
                 container.css( "width", elt.width() );
+                container.attr( "id", this.spinner_id() );
 
                 var spinner = this.my.Spinner().clone();
                 if ( elt.height() > spinner.height() )  {
@@ -73,12 +82,24 @@ Role(
 
                 this.spinner(container);
             },
+            _elt_needs_spinner: function (elt) {
+                if ( ! elt.length ) {
+                    return false;
+                }
+
+                if ( elt.find( "#" + this.spinner_id() ).length ) {
+                    return false;
+                }
+
+                return true;
+            },
             _removeSpinner: function () {
                 if ( ! this.spinner() ) {
                     return;
                 }
 
                 this.spinner().detach();
+                this.spinner(null);
             }
         }
     }
